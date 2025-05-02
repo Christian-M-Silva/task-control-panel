45e99ca013ee4ea63bd2ceae6564771beda1e3f9
Christian Silva <tininjacms@gmail.com>
2025-05-02 14:07:27 -0300

    feat(finalizarProject): Finalizará projeto

--- src/components/TaskDashboardComponent/index.tsx
+++ src/components/TaskDashboardComponent/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useTaskContext } from "../../contexts/TaskContext";
 import { StatusType } from "../../tipagens/types";
 import { Card } from "../CardComponent";
@@ -8,9 +9,18 @@ export function TaskDashboard() {
         return status === 'concluida'
     }
 
+    const totalTasks = useMemo(() => {
+        return tasks.length
+      }, [tasks.length]);
+
+
     return (
         <>
             {tasks.map(task => <Card description={task.description} id={task.id} isChecked={isChecked(task.status)} priority={task.priority} status={task.status} title={task.title}></Card>)}
+
+            <span className="text-emerald-500">
+                Total de tasks: {totalTasks}
+            </span>
         </>
     )
 }
