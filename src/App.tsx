758e7c70756903a0917b60a8d376dd4080c1e128
Christian Silva <tininjacms@gmail.com>
2025-05-02 12:22:26 -0300

    feat(addTaskBoard): AddTaksBoard na tela do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import { useState } from "react"
 import { Modal } from "./components/ModalComponent"
+import { TaskDashboard } from "./components/TaskDashboardComponent"
 
-// import { Card } from "./components/CardComponent"
 
 function App() {
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
 
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col justify-center">
       <button
         className="mt-4 bg-green-700 text-white p-4 rounded-lg hover:bg-green-950 transition cursor-pointer"
         onClick={() => setIsOpenModal((prevValue) => !prevValue)}
       >
         Adicionar tarefas
       </button>
-      <Modal isOpen={isOpenModal} onClose={() => setIsOpenModal((prevValue) => !prevValue)} />
-
-      {/* <Card/> */}
+      <Modal isOpen={isOpenModal} onClose={() => setIsOpenModal((prevValue) => !prevValue)}  />
+      <TaskDashboard />
     </div>
 
 
--- src/components/TaskDashboardComponent/index.tsx
+++ src/components/TaskDashboardComponent/index.tsx
@@ -10,11 +10,6 @@ export function TaskDashboard() {
 
     return (
         <>
-            <button
-                className="mt-4 bg-red-700 text-white py-2 rounded-lg hover:bg-red-950 transition cursor-pointer"
-            >
-                Limpar modal
-            </button>
             {tasks.map(task => <Card description={task.descriptions} id={task.id} isChecked={isChecked(task.status)} priority={task.priority} status={task.status} title={task.title}></Card>)}
         </>
     )
