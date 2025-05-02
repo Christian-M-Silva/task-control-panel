a52842671c0847cf544db00ebb3d709b7a163d7f
Christian Silva <tininjacms@gmail.com>
2025-05-02 11:02:47 -0300

    feat(mapTaskBoard): ncluirá o .map dos dados do context

--- src/components/TaskDashboardComponent/index.tsx
+++ src/components/TaskDashboardComponent/index.tsx
@@ -1,7 +1,16 @@
+import { useTaskContext } from "../../contexts/TaskContext";
 import { StatusType } from "../../tipagens/types";
+import { Card } from "../CardComponent";
 
 export function TaskDashboard() {
+    const {tasks} = useTaskContext()
     function isChecked(status:StatusType): boolean {
         return status === 'concluida' 
     }
+
+    return (
+        <>
+            {tasks.map(task => <Card description={task.descriptions} id={task.id} isChecked={isChecked(task.status)} priority={task.priority} status={task.status} title={task.title}></Card>)}
+        </>
+    )
 }
